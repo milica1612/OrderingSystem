b0b36c2483951cb7bfc831b761f9276b8f1cff6a
milica1612 <lukic.ra17.2017@uns.ac.rs>
2021-12-06 20:51:49 +0100

    Spark library added

--- WebProject/src/main/Main.java
+++ WebProject/src/main/Main.java
@@ -1,10 +1,26 @@
 package main;
 
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.staticFiles;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.google.gson.Gson;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private static Gson g = new Gson();
+	
+	public static void main(String[] args) throws Exception {
+		port(8088);
+		
+		staticFiles.externalLocation(new File("./static").getCanonicalPath()); 
+		
+		get("/test", (req, res) -> {
+			return "Works";
+		});
 	}
 
 }
